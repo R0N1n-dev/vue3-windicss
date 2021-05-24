7c1402d25ad5708ce17d04a266c46e1ee952686f
Richard Killingsworth <richardk80@icloud.com>
2021-05-24 13:32:25 -0500

    Commit 12

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,7 @@
         >vue-cli documentation</a
       >.
     </p>
-    <h3>Styled with Tailwindcss</h3>
+    <h3>Styled with Tailwindcss using @apply</h3>
     <h3>Essential Links</h3>
     <ul>
       <li>
