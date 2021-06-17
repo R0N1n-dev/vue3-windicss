5ac4a64c689cbed5214e3d6f45ecee35328603e3
Richard Killingsworth <richardk80@icloud.com>
2021-06-17 18:47:53 -0500

    Commit 41

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <img class="logo" alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
@@ -17,9 +16,5 @@ export default {
 <style>
 #app {
   @apply antialiased text-center text-gray-800 text-base mt-10 font-semibold;
-}
-
-.logo {
-  @apply inline-block text-center;
 }
 </style>
