22aa5c04e6df29e0960fc009491b5cc127ac37c8
Richard Killingsworth <richardk80@icloud.com>
2021-06-17 19:17:20 -0500

    Commit 46

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <Logo class="logo" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <svg
+      class="logo"
+      viewBox="0 0 261.76 226.69"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g transform="matrix(1.3333 0 0 -1.3333 -76.311 313.34)">
+        <g transform="translate(178.06 235.01)">
+          <path
+            d="m0 0-22.669-39.264-22.669 39.264h-75.491l98.16-170.02 98.16 170.02z"
+            fill="#41b883"
+          />
+        </g>
+        <g transform="translate(178.06 235.01)">
+          <path
+            d="m0 0-22.669-39.264-22.669 39.264h-36.227l58.896-102.01 58.896 102.01z"
+            fill="#34495e"
+          />
+        </g>
+      </g>
+    </svg>
+    <p>Vue 3 and Windi CSS using @apply</p>
+  </div>
 </template>
-
-<script>
-import Logo from './assets/Logo.vue'
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Logo,
-    HelloWorld,
-  },
-}
-</script>
 
 <style>
 #app {
   @apply antialiased text-center text-gray-800 mt-10 font-semibold;
 }
 .logo {
-  @apply inline-block text-center;
+  @apply inline-block text-center h-50 w-50;
+}
+p {
+  @apply mt-10 mb-0 mx-0 font-semibold;
 }
 </style>
